34f79c0ff83cbc39798f71cc1fb8459df1154bc6
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-12-12 15:11:16 -0300

    asd

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,60 +1,62 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe contener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
+    validarNombre("") === "Este campo debe contener al menos 1 caracter",
+    "Validar nombre no validó que el nombre no sea vacío"
   );
 
   console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    validarNombre(
+      "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    ) === "Este campo debe tener menos de 50 caracteres",
+    "Validar nombre no validó que el nombre sea menor a 50 caracteres"
   );
 
   console.assert(
-      validarNombre('asda123123123') === 'Este campo solo acepta letras',
-      'Validar nombre no valido que el nombre solo tenga letras'
+    validarNombre("asda123123123") === "Este campo solo acepta letras",
+    "Validar nombre no valido que el nombre solo tenga letras"
   );
 
   console.assert(
-      validarNombre('Emmanuel') === '',
-      'validarNombre no funciono con un nombre valido'
+    validarNombre("Emmanuel") === "",
+    "validarNombre no funciono con un nombre valido"
   );
 }
 
 function probarValidarProvincia() {
-    console.assert(
-        validarProvincia('') === 'Este campo debe contener una provincia',
-        'validarProvincia no mosotro el mensaje de completar campo con una provincia' 
-    );
+  console.assert(
+    validarProvincia("") === "Este campo debe contener una provincia",
+    "validarProvincia no mosotro el mensaje de completar campo con una provincia"
+  );
 
-    console.assert(
-        validarProvincia('Rio Negro') === '',
-        'validarProvincia no funciono con un nombre de provincia valido'
-    );
+  console.assert(
+    validarProvincia("Rio Negro") === "",
+    "validarProvincia no funciono con un nombre de provincia valido"
+  );
 }
 
-function probarValidarDescripcionRegalo () {
-    console.assert(
-        validarDescripcionRegalo('') === 'El campo no puede estar vacio',
-        'validarDescripcionRegalo no valido que el campo no pueda ser vacio'
-    );
+function probarValidarDescripcionRegalo() {
+  console.assert(
+    validarDescripcionRegalo("") === "El campo no puede estar vacio",
+    "validarDescripcionRegalo no valido que el campo no pueda ser vacio"
+  );
 
-    console.assert(
-        validarDescripcionRegalo('11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111')
-        === 'El campo no puede contener mas de 100 caracteres',
-        'validarDescripcionRegalo no valido que el campo no pueda contener mas de 100 caracteres'
-    );
+  console.assert(
+    validarDescripcionRegalo(
+      "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111"
+    ) === "El campo no puede contener mas de 100 caracteres",
+    "validarDescripcionRegalo no valido que el campo no pueda contener mas de 100 caracteres"
+  );
 
-    console.assert(
-        validarDescripcionRegalo('Quiero un regalo') === '',
-        'validarDescripcionRegalo no funciono con una descripcion de regalo valida'
-    );
+  console.assert(
+    validarDescripcionRegalo("Quiero un regalo") === "",
+    "validarDescripcionRegalo no funciono con una descripcion de regalo valida"
+  );
 
-    console.assert(
-        validarDescripcionRegalo(',.,.,.,') === 'El campo solo puede contener letras y numeros',
-        'validarDescripcionRegalo no valido que el campo solo acepte letras y numeros'
-    );
+  console.assert(
+    validarDescripcionRegalo(",.,.,.,") ===
+      "El campo solo puede contener letras y numeros",
+    "validarDescripcionRegalo no valido que el campo solo acepte letras y numeros"
+  );
 }
 
 probarValidarNombre();
